760abdd73fe51ca57dc1e3891d86ae1820bfb0fd
comegender <3517999384@qq.com>
2025-11-23 11:08:33 +0800

    更正脚本语言读入处理逻辑

--- backend/test_script.py
+++ backend/test_script.py
@@ -1,12 +1,21 @@
 f = open("./脚本语言设计.md", "r", encoding='utf-8').read()
 s=f.split('### ')[9:]
 d={}
+de={}
 
 for i in range(len(s)):
     s[i]=s[i].split("```")
 
+
 for i in s:
     d[i[0].strip()]=i[1][9:]
+    
+for i in d:
+    ss = d[i].split(' ')
+    de[ss[1]]=d[i]
 
 def get_script(name):
     return d.get(name, "")
+
+def get_script_en(name):
+    return de.get(name, "")
